fcfc527c6dcf3f9e7f943e3a316d48c6c150ced6
Marcelo Valls <claudio.valls@despegar.com>
2020-02-26 00:35:48 -0300

    Fix url globals

--- src/app/services/Globals.ts
+++ src/app/services/Globals.ts
@@ -1,6 +1,6 @@
 export class Globals {
 
-    //static BACKEND_HOST = "https://sidged-be.herokuapp.com";
-    static BACKEND_HOST = "http://localhost:8080";
+    static BACKEND_HOST = "https://sidged-be.herokuapp.com";
+    //static BACKEND_HOST = "http://localhost:8080";
 
 }
